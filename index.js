b19c68e085fda3e89bc7cf0a8fb686de615e2692
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-06-17 11:27:56 -0400

    add function to render staff

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var c;
+var ctx;
+
+var WIDTH = 800, HEIGHT = 300;
+
+function init() {
+    c = document.getElementById("mainCanvas");
+    c.width = WIDTH;
+    c.height = HEIGHT;
+    ctx = c.getContext("2d");
+}
+
+function line(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+var lineGapHeight = 20;
+
+function drawStaff(x, y, length) {
+    for (var i = 0; i < 5; i++) {
+        console.log("X: %s, Y: %s, length: %s", x,y,length);
+        line(x, y, x + length, y);
+        y += lineGapHeight;
+    }
+}
+
+init();
+drawStaff(25, 25, 750);
+drawStaff(25, 150, 750);
