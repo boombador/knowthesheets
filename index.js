9131cf1f58957e05a5e88c43f358905e48305859
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-06-17 15:32:25 -0400

    add logic for animation loop, though still static

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 var c;
 var ctx;
+var staff;
 
 var WIDTH = 800, HEIGHT = 300;
 var TAIL_HEIGHT = 60;
@@ -10,6 +11,42 @@ function init() {
     c.width = WIDTH;
     c.height = HEIGHT;
     ctx = c.getContext("2d");
+
+    staff = new Staff(25, 150, 750, 20, 50);
+
+    staff.addNote(0, 0, 1); // C
+    staff.addNote(0, 1, 1); // D
+    staff.addNote(0, 2, 1); // E
+    staff.addNote(0, 3, 1); // F
+    staff.addNote(0, 4, 1); // G
+    staff.addNote(0, 5, 1); // A
+    staff.addNote(0, 6, 1); // B
+
+    staff.addNote(-1, 7, 1);
+    staff.addNote(-1, 6, 1);
+    staff.addNote(-1, 5, 1);
+    staff.addNote(-1, 4, 1);
+    staff.addNote(-1, 3, 1);
+    staff.addNote(-1, 2, 1);
+    staff.addNote(-1, 1, 1);
+
+    staff.processNote();
+    staff.processNote();
+
+    loop();
+}
+
+//document.body.onkeyup = function(e){
+    //if(e.keyCode == 32){
+        ////your code
+    //}
+//}
+
+function loop() {
+    requestAnimationFrame(loop);
+
+    ctx.clearRect( 0, 0, WIDTH, HEIGHT );
+    staff.render();
 }
 
 function line(x1, y1, x2, y2) {
@@ -60,6 +97,7 @@ function Staff(x, y, length, lineGap, beatDist) {
     
     this.currentX = x;
     this.notes = [];
+    this.activeNote = 0;
 }
 
 Staff.prototype.render = function() {
@@ -73,14 +111,28 @@ Staff.prototype.render = function() {
         y -= this.lineGap;
     }
 
+    // bass staff
     y = staffY + this.lineGap;
     for (i = 0; i < 5; i++) {
         line(staffX, y, staffX + this.length, y);
         y += this.lineGap;
     }
+
+    // draw notes
     var noteRadius = this.stepHeight;
+    var activeIndex = this.activeNote;
     this.notes.map(function(note, i) {
+        if (activeIndex == i) {
+            ctx.save();
+            ctx.fillStyle = "rgb(255,165,0)";
+            ctx.strokeStyle = "rgb(255,165,0)";
+        }
+
         drawNote( note.x, note.y, note.duration, noteRadius );
+
+        if (activeIndex == i) {
+            ctx.restore();
+        }
     });
 };
 
@@ -96,23 +148,8 @@ Staff.prototype.addNote = function(octave, note, duration) {
     this.currentX += this.beatDist * duration;
 };
 
+Staff.prototype.processNote = function() {
+    this.activeNote++;
+};
+
 init();
-var staff = new Staff(25, 150, 750, 20, 50);
-
-staff.addNote(0, 0, 1); // C
-staff.addNote(0, 1, 1); // D
-staff.addNote(0, 2, 1); // E
-staff.addNote(0, 3, 1); // F
-staff.addNote(0, 4, 1); // G
-staff.addNote(0, 5, 1); // A
-staff.addNote(0, 6, 1); // B
-
-staff.addNote(-1, 7, 1);
-staff.addNote(-1, 6, 1);
-staff.addNote(-1, 5, 1);
-staff.addNote(-1, 4, 1);
-staff.addNote(-1, 3, 1);
-staff.addNote(-1, 2, 1);
-staff.addNote(-1, 1, 1);
-
-staff.render();
