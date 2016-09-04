9bd2eb1a8e8190e5d3a2ccd2d4b6e17241f43284
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-09-04 17:28:52 -0400

    demo with webpack-dev-server instead of http-server

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+
+var path = require('path');
+
+module.exports = {
+  entry: "./app.js",
+  output: {
+      path: path.resolve(__dirname, "build"),
+      publicPath: "/assets/",
+      filename: "bundle.js"
+  },
+  watch: true
+}
